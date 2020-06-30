4f4fc24775dc70869c068d46a168b5d80c1ce836
MeatReed <meatreeed@gmail.com>
2020-06-30 16:28:55 +0200

    improve appearence page & add wallpaper/blur img

--- src/dashboard/pages/appearance.vue
+++ src/dashboard/pages/appearance.vue
@@ -1,20 +1,37 @@
 <template>
   <v-container>
     <h1>Appearance</h1>
+    <h2 class="ml-3">Wallpaper</h2>
     <v-row>
-      <v-col v-for="(item, index) of items" :key="index">
+      <v-col v-for="(item, index) of wallpaper" :key="index">
         <v-card
           v-if="item"
           class="mx-auto"
           color="transparent"
           width="244"
           outlined
-          @click="$colorMode.preference = `image${index + 1}`"
+          @click="$colorMode.preference = `wallpaper${index + 1}`"
         >
           <v-img class="white--text align-end" :src="item" />
         </v-card>
       </v-col>
     </v-row>
+    <h2 class="ml-3">Blur</h2>
+    <v-row>
+      <v-col v-for="(item, index) of blur" :key="index">
+        <v-card
+          v-if="item"
+          class="mx-auto"
+          color="transparent"
+          width="244"
+          outlined
+          @click="$colorMode.preference = `blur${index + 1}`"
+        >
+          <v-img class="white--text align-end" :src="item" />
+        </v-card>
+      </v-col>
+    </v-row>
+    <h2 class="ml-3">Color</h2>
     <v-row>
       <v-col class="text-center">
         <v-card
@@ -38,6 +55,61 @@
           <h1>LIGHT</h1>
         </v-card>
       </v-col>
+      <v-col class="text-center">
+        <v-card
+          class="mx-auto"
+          color="transparent"
+          width="244"
+          outlined
+          @click="$colorMode.preference = `blue`"
+        >
+          <h1>BLUE</h1>
+        </v-card>
+      </v-col>
+      <v-col class="text-center">
+        <v-card
+          class="mx-auto"
+          color="transparent"
+          width="244"
+          outlined
+          @click="$colorMode.preference = `pink`"
+        >
+          <h1>PINK</h1>
+        </v-card>
+      </v-col>
+      <v-col class="text-center">
+        <v-card
+          class="mx-auto"
+          color="transparent"
+          width="244"
+          outlined
+          @click="$colorMode.preference = `green`"
+        >
+          <h1>GREEN</h1>
+        </v-card>
+      </v-col>
+      <v-col class="text-center">
+        <v-card
+          class="mx-auto"
+          color="transparent"
+          width="244"
+          outlined
+          @click="$colorMode.preference = `purple`"
+        >
+          <h1>PURPLE</h1>
+        </v-card>
+      </v-col>
+      <v-col class="text-center">
+        <v-card
+          class="mx-auto"
+          color="transparent"
+          width="244"
+          outlined
+          @click="$colorMode.preference = `yellow`"
+        >
+          <h1>YELLOW</h1>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -47,7 +119,7 @@ export default {
   data() {
     return {
       botinfo: null,
-      items: [
+      wallpaper: [
         require('~/assets/images/wallpaper/1.jpg'),
         require('~/assets/images/wallpaper/2.png'),
         require('~/assets/images/wallpaper/3.jpg'),
@@ -57,6 +129,16 @@ export default {
         require('~/assets/images/wallpaper/7.png'),
         require('~/assets/images/wallpaper/8.jpg'),
         require('~/assets/images/wallpaper/9.jpg'),
+        require('~/assets/images/wallpaper/10.jpg'),
+        require('~/assets/images/wallpaper/11.jpg'),
+        require('~/assets/images/wallpaper/12.jpg'),
+        require('~/assets/images/wallpaper/13.jpg'),
+      ],
+      blur: [
+        require('~/assets/images/blur/1.jpg'),
+        require('~/assets/images/blur/2.jpg'),
+        require('~/assets/images/blur/3.jpg'),
+        require('~/assets/images/blur/4.jpg'),
       ],
     }
   },
